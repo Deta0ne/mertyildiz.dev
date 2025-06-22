74eaecfc63eb5871dc62610c5ebaf6fe7e6e1d4d
Mert Yıldız <mert@Mert-MacBook-Pro.local>
2025-06-22 19:41:34 +0300

    feat: Created project folder

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Web Developer</h1>
+                <Button>Click me</Button>
+            </div>
+        </div>
+    );
+}
